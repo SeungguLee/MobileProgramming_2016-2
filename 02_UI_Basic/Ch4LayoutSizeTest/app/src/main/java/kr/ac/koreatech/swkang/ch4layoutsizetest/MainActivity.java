bafa4bf76c98a312359a2c195d04d89688ff2d7f
Seungwoo Kang <swkang@koreatech.ac.kr>
2016-09-07 22:28:11 +0900

    additional user interface example projects included

---
+++ 02_UI_Basic/Ch4LayoutSizeTest/app/src/main/java/kr/ac/koreatech/swkang/ch4layoutsizetest/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.koreatech.swkang.ch4layoutsizetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
