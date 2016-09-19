a785ad13958c068b95afe97a69f710e6017ca5ee
Seungwoo Kang <swkang@koreatech.ac.kr>
2016-09-19 14:41:14 +0900

    event handling examples added

---
+++ 03_Event_Handling/Ch6RadioButton/app/src/main/java/kr/ac/koreatech/swkang/ch6radiobutton/MainActivity.java
@@ -0,0 +1,31 @@
+package kr.ac.koreatech.swkang.ch6radiobutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.radio_red:
+                if(checked)
+                    Toast.makeText(getApplicationContext(), ((RadioButton) view).getText(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.radio_blue:
+                if(checked)
+                    Toast.makeText(getApplicationContext(), ((RadioButton) view).getText(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
